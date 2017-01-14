044bba9497fa785f0db3f4fcdd3abf09a598b569
Guzman Monne <guzmonne@hotmail.com>
2017-01-14 13:27:17 -0300

    added margins to line chart

--- src/components/LineChart.js
+++ src/components/LineChart.js
@@ -9,8 +9,9 @@ import ChartFaC from './ChartFaC.js'
 import TooltipFaC from './TooltipsFaC.js'
 import Tooltip from './LineChartTooltip.js'
 
-const LineChart = (props) => {
+const LineChart = (props) => {  
   const {data, fill, stroke, tooltips} = props
+  
   const linePoints = (data, xScale, yScale) => (
     d3.line()
       .x(d => xScale(d[0]))
@@ -18,6 +19,7 @@ const LineChart = (props) => {
       .curve(d3.curveCatmullRom.alpha(0.5))
       (data)
   )
+
   return (
     <div className="LineChart">
       <TooltipFaC>
@@ -27,10 +29,11 @@ const LineChart = (props) => {
                   tooltip={tooltips ? <Tooltip {...tooltipState} /> : <none />}
                   {...props}>
           {({xScale, yScale, w, h}) => (
-            <g>
+            <g className="LineChart__path"> 
               <path d={linePoints(data, xScale, yScale)} 
                     fill="transparent"
-                    stroke={stroke}/>
+                    stroke={stroke}
+                    strokeWidth="2"/>
             {data.map((d, i) => (
               <g key={`Circles.${i}`}>
                 <circle cx={xScale(d[0])} 
@@ -51,13 +54,19 @@ const LineChart = (props) => {
             ))}
             </g>
           )}</ChartFaC>
-      )}</TooltipFaC> 
+      )}</TooltipFaC>
     </div>
   )
 }
 
 LineChart.propTypes = {
   data: T.arrayOf(T.array).isRequired,
+  margin: T.shape({
+    top: T.number,
+    right: T.number,
+    bottom: T.number,
+    left: T.number,
+  }),
   fill: T.string,
   stroke: T.string,
   pointFill: T.string,
@@ -65,6 +74,7 @@ LineChart.propTypes = {
 }
 
 LineChart.defaultProps = {
+  margin: {top: 30, right: 15, bottom: 40, left: 30},
   tooltips: true,
   stroke: '#5B5F97',
   fill: '#5B5F97',
